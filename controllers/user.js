e6bedb47840be8cf2bf49f67872a68c90ac7661f
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-08-17 21:17:26 +0530

    unfollow part is added in follow section

--- controllers/user.js
+++ controllers/user.js
@@ -98,16 +98,33 @@ exports.followUser = async (req, res) => {
       });
     }
 
-    loggedInUser.following.push(userToFollow._id);
-    userToFollow.followers.push(loggedInUser._id);
+    if (loggedInUser.following.includes(userToFollow._id)) {
+      const indexToFollow = loggedInUser.following.indexOf(userToFollow._id);
 
-    await loggedInUser.save();
-    await userToFollow.save();
+      const indexOfLoggedIn = userToFollow.followers.indexOf(loggedInUser._id);
 
-    res.status(200).json({
-      success: true,
-      message: "User Followed !",
-    });
+      userToFollow.followers.splice(indexOfLoggedIn, 1);
+
+      loggedInUser.following.splice(indexToFollow, 1);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+      res.status(401).json({
+        success: true,
+        message: "User is Unfollowed !",
+      });
+    } else {
+      loggedInUser.following.push(userToFollow._id);
+      userToFollow.followers.push(loggedInUser._id);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({
+        success: true,
+        message: "User Followed !",
+      });
+    }
   } catch (error) {
     res.status(500).json({
       success: false,
