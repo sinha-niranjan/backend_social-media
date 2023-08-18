624992749b30a79fdfedd64ee484146791cd864c
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-08-18 23:27:48 +0530

    comment functionallit in post

--- controllers/post.js
+++ controllers/post.js
@@ -179,3 +179,105 @@ exports.updateCaption = async (req, res) => {
     });
   }
 };
+
+exports.commentOnPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found !",
+      });
+    }
+
+    let commentIndex = -1;
+
+    post.comments.forEach((item, index) => {
+      if (item.user.toString() === req.user._id.toString()) {
+        commentIndex = index;
+      }
+    });
+
+    if (commentIndex !== -1) {
+      post.comments[commentIndex].comment = req.body.comment;
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Comment is Updated !",
+      });
+    } else {
+      post.comments.push({
+        user: req.user._id,
+        comment: req.body.comment,
+      });
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Comment added",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found !",
+      });
+    }
+
+    if (post.owner.toString() === req.user._id.toString()) {
+      if (req.body.commentId === undefined) {
+        return res.status(400).json({
+          success: false,
+          message: "Comment id is required ! ",
+        });
+      }
+
+      post.comments.forEach((item, index) => {
+        if (item._id.toString() === req.body.commentId.toString()) {
+          return post.comments.splice(index, 1);
+        }
+      });
+
+      await post.save();
+
+      return res.status(200).json({
+        success: false,
+        message: "Selected comment is deleted ! ",
+      });
+    } else {
+      post.comments.forEach((item, index) => {
+        if (item.user.toString() === req.user._id.toString()) {
+          return post.comments.splice(index, 1);
+        }
+      });
+
+      await post.save();
+
+      res.status(200).json({
+        success: false,
+        message: "Your Comment is deleted",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,8 @@ const {
   deletePost,
   getPostOfFollowing,
   updateCaption,
+  commentOnPost,
+  deleteComment,
 } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
@@ -15,9 +17,14 @@ router.route("/post/upload").post(isAuthenticated, createPost);
 router
   .route("/post/:id")
   .get(isAuthenticated, likeAndUnlikePost)
-  .put(isAuthenticated,updateCaption)
+  .put(isAuthenticated, updateCaption)
   .delete(isAuthenticated, deletePost);
 
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
+
+router
+  .route("/post/comment/:id")
+  .put(isAuthenticated, commentOnPost)
+  .delete(isAuthenticated, deleteComment);
 
 module.exports = router;
